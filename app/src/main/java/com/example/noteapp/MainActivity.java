ad82a10e3dce76f72d99135b4a622a3e97cf806c
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2022-12-01 21:32:50 +0700

    Progress ini: menambahkan res menu popup dan fitur pin/unpin dan delete

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.noteapp;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.SearchView;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,7 +12,9 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.noteapp.Adapters.NotesListAdapter;
 import com.example.noteapp.Database.RoomDB;
@@ -21,13 +24,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     RecyclerView recyclerView;
     NotesListAdapter notesListAdapter;
     List<Notes> notes = new ArrayList<>();
     RoomDB database;
     FloatingActionButton fab_add;
     SearchView searchView_home;
+    Notes selectedNote;
 
 
     @Override
@@ -119,7 +123,46 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onLongClick(Notes notes, CardView cardView) {
-
+            selectedNote = new Notes();
+            selectedNote = notes;
+            showPopup(cardView);
         }
     };
+
+    private void showPopup(CardView cardView) {
+        PopupMenu popupMenu = new PopupMenu(this, cardView);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.pin:
+                if (selectedNote.isPinned()){
+                    database.mainDAO().pin(selectedNote.getID(), false);
+                    Toast.makeText(MainActivity.this, "Unpinned!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    database.mainDAO().pin(selectedNote.getID(), true);
+                    Toast.makeText(MainActivity.this, "Pinned!", Toast.LENGTH_SHORT).show();
+                }
+
+                notes.clear();
+                notes.addAll(database.mainDAO().getAll());
+                notesListAdapter.notifyDataSetChanged();
+                return true;
+
+            case R.id.delete:
+                database.mainDAO().delete(selectedNote);
+                notes.remove(selectedNote);
+                notesListAdapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this, "Note Deleted!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return false;
+        }
+    }
 }
